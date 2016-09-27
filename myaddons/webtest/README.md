4fe64bf015b28b9a26cf0aa7e4b378587f8832ea
wanghaipeng28 <wanghaipeng28@163.com>
2016-09-27 16:56:51 +0800

    updata myaddons

--- myaddons/webtest/README.md
+++
@@ -1,2 +0,0 @@
-# webstyle
-nantianDP web style 
--- myaddons/webtest/controllers/main.py
+++
@@ -1,9 +0,0 @@
-#coding:utf8
-
-from openerp import http
-
-class WebTst(http.Controller):
-
-    @http.route("/web", auth="none", web=True)
-    def webtst(self):
-        return http.request.render("webtest.idname", {"title":"hello word","uname":["wanghaipeng","lihailiang","linana","gaoyan","fengyan","libaokun"],"uage":23})
