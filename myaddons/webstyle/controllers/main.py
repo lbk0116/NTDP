76ea2559e99fa5c96c23c8ffcc83c9b774d51aaf
wanghaipeng28 <wanghaipeng28@163.com>
2016-09-27 17:03:05 +0800

    delete myaddons

--- myaddons/webstyle/controllers/main.py
+++
@@ -1,9 +0,0 @@
-#coding:utf8
-
-from openerp import http
-
-class WebStyle(http.Controller):
-
-    @http.route("/web", auth="none", web=True)
-    def webstyle(self):
-        return http.request.render("webstyle.idname", {})
